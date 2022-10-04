100b440ef987d6142404084bda00bca7cfa65141
Dirk Arnez <smalldirkalex@gmail.com>
2022-10-05 03:38:29 +0800

    - add local-run.sh

---
+++ local-run.sh
@@ -0,0 +1,5 @@
+export PATH="/usr/local/bin:/usr/bin:/bin:/d/Softwares/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64:/d/Softwares/x86_64-8.1.0-release-posix-seh-rt_v6-rev0/mingw64/bin" &&
+
+gcc --version
+
+read -p "End"
